7be5dc585c7dc7dffe0e8202f5656fa9de5ba335
clroot <abcdkh1209@gmail.com>
2021-11-16 19:55:39 +0900

    add Thymeleaf layout example code

--- src/main/java/io/clroot/study/shop/controller/ThymeleafExController.java
+++ src/main/java/io/clroot/study/shop/controller/ThymeleafExController.java
@@ -82,4 +82,9 @@ public class ThymeleafExController {
 
         return "thymeleafEx/thymeleafEx06";
     }
+
+    @GetMapping("/ex07")
+    public String thymeleafEx07() {
+        return "thymeleafEx/thymeleafEx07";
+    }
 }
