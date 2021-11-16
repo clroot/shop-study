20b9e3202ecbb1dda4e660741a80ac7f507e4951
clroot <abcdkh1209@gmail.com>
2021-11-16 10:51:08 +0900

    Init Project

---
+++ src/test/java/io/clroot/study/shop/ShopApplicationTests.java
@@ -0,0 +1,12 @@
+package io.clroot.study.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
