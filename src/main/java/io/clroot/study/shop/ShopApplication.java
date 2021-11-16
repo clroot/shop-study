2cd3ab79410f28e5de7932afcfbc2b63f5cc14f0
clroot <abcdkh1209@gmail.com>
2021-11-16 13:36:44 +0900

    enable jpa auditing
    - add @EnableJpaAuditing to ShopApplication class
    - add @EntityListeners to Item Entity class

--- src/main/java/io/clroot/study/shop/ShopApplication.java
+++ src/main/java/io/clroot/study/shop/ShopApplication.java
@@ -2,8 +2,10 @@ package io.clroot.study.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ShopApplication {
 
     public static void main(String[] args) {
