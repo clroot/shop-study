51917ad15fc7cb9708c6d01626790e73288c6f8c
clroot <abcdkh1209@gmail.com>
2021-11-17 23:49:54 +0900

    implement login
    - using UserDetailsService

--- src/main/java/io/clroot/study/shop/controller/MemberController.java
+++ src/main/java/io/clroot/study/shop/controller/MemberController.java
@@ -46,4 +46,15 @@ public class MemberController {
         model.addAttribute("memberFormDTO", new MemberFormDTO());
         return "member/memberForm";
     }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
+        return "member/memberLoginForm";
+    }
 }
