8570794b748935ad5813b2f3e5fc56a896eff56b
clroot <abcdkh1209@gmail.com>
2021-11-18 00:01:36 +0900

    write MemberControllerTest
    - loginSuccessTest
    - loginFailTest
    - using spring-security-test

---
+++ src/test/java/io/clroot/study/shop/controller/MemberControllerTest.java
@@ -0,0 +1,71 @@
+package io.clroot.study.shop.controller;
+
+import io.clroot.study.shop.dto.MemberFormDTO;
+import io.clroot.study.shop.entity.Member;
+import io.clroot.study.shop.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class MemberControllerTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Member createMember(String email, String password) {
+        MemberFormDTO memberFormDTO = new MemberFormDTO();
+        memberFormDTO.setEmail(email);
+        memberFormDTO.setPassword(password);
+        memberFormDTO.setName("테스트");
+        memberFormDTO.setAddress("서울시 강남구");
+
+        Member member = Member.createMember(memberFormDTO, passwordEncoder);
+
+        return memberService.saveMember(member);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    public void loginSuccessTest() throws Exception {
+        String email = "test@email.com";
+        String password = "password";
+        this.createMember(email, password);
+
+        mockMvc.perform(formLogin().userParameter("email")
+                        .loginProcessingUrl("/members/login")
+                        .user(email).password(password))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    public void loginFailTest() throws Exception {
+        String email = "test@email.com";
+        String password = "password";
+        this.createMember(email, password);
+
+        mockMvc.perform(formLogin().userParameter("email")
+                        .loginProcessingUrl("/members/login")
+                        .user(email).password("wrongPassword"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
+}
