6447e050ecef322ed7562051965f6d178f2f4910
clroot <abcdkh1209@gmail.com>
2021-11-17 17:17:58 +0900

    implement Member Entity & DTO

---
+++ src/main/java/io/clroot/study/shop/dto/MemberFormDTO.java
@@ -0,0 +1,17 @@
+package io.clroot.study.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberFormDTO {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String address;
+}
