13747b63d253ef41cdb77146a09e66f98ae74c69
clroot <abcdkh1209@gmail.com>
2021-11-19 13:00:06 +0900

    implement ItemRegister Form

--- src/main/java/io/clroot/study/shop/controller/ItemController.java
+++ src/main/java/io/clroot/study/shop/controller/ItemController.java
@@ -1,13 +1,17 @@
 package io.clroot.study.shop.controller;
 
+import io.clroot.study.shop.dto.ItemFormDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
 
     @GetMapping("/admin/item/new")
-    public String itemForm() {
+    public String itemForm(Model model) {
+        model.addAttribute("itemFormDTO", new ItemFormDTO());
+
         return "item/itemForm";
     }
 }
