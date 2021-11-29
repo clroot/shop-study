879119859707b61232a7f2e96f9ffbf6fbbddd3d
clroot <abcdkh1209@gmail.com>
2021-11-29 17:15:36 +0900

    ItemImgService 구현

---
+++ src/main/java/io/clroot/study/shop/repository/ItemImgRepository.java
@@ -0,0 +1,8 @@
+package io.clroot.study.shop.repository;
+
+import io.clroot.study.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+}
