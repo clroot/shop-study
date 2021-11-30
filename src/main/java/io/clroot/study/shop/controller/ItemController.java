9fd8fd3a8d00275dfbbd65ea1f1ac226389af06e
clroot <abcdkh1209@gmail.com>
2021-11-30 15:01:07 +0900

    ItemController 구현

--- src/main/java/io/clroot/study/shop/controller/ItemController.java
+++ src/main/java/io/clroot/study/shop/controller/ItemController.java
@@ -1,17 +1,53 @@
 package io.clroot.study.shop.controller;
 
 import io.clroot.study.shop.dto.ItemFormDTO;
+import io.clroot.study.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
 
     @GetMapping("/admin/item/new")
     public String itemForm(Model model) {
         model.addAttribute("itemFormDTO", new ItemFormDTO());
 
         return "item/itemForm";
+    }
+
+    @PostMapping("/admin/item/new")
+    public String itemSubmit(@Valid ItemFormDTO itemFormDTO, BindingResult bindingResult,
+                             @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList,
+                             Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "item/itemForm";
+        }
+
+        if (itemImgFileList.get(0).isEmpty() && itemFormDTO.getId() == null) {
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 핋수 입력 값 입니다.");
+            return "item/itemForm";
+        }
+
+        try {
+            itemService.saveItem(itemFormDTO, itemImgFileList);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
+            return "item/itemForm";
+        }
+
+        return "redirect:/";
     }
 }
